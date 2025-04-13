b5929b7664ff808db88a229d19edfbd736e9b7cc
ShahFaisalIslam <faisal.islam.ceme@gmail.com>
2025-04-14 00:16:18 +0500

    Ensured that retrieve data works only when data already exists

--- retrieve_data.py
+++ retrieve_data.py
@@ -4,17 +4,21 @@ st.subheader("Retrieve Data")
 st.write("Did you store your data with us? Fetch it by confirming your\
  passkey!")
 
-passkey = st.text_input("Passkey:")
+if "key" in st.session_state["stored_data"]:
 
-if st.button("Fetch Data"):
-    if passkey:
-        if st.session_state["stored_data"]["passkey"] == gen_secure_hash(passkey,st.session_state["stored_data"]["key"]):
-            st.text_input(label="Data:",disabled=True,
-                        value=decrypt(st.session_state["stored_data"]["data"],
-                                    st.session_state["stored_data"]["key"]))
+    passkey = st.text_input("Passkey:")
+
+    if st.button("Fetch Data"):
+        if passkey:
+            if st.session_state["stored_data"]["passkey"] == gen_secure_hash(passkey,st.session_state["stored_data"]["key"]):
+                st.text_input(label="Data:",disabled=True,
+                            value=decrypt(st.session_state["stored_data"]["data"],
+                                        st.session_state["stored_data"]["key"]))
+            else:
+                "Invalid passkey, try again"
         else:
-            "Invalid passkey, try again"
-    else:
-        "Please enter passkey before fetching data!"
-# if st.button("Test Login"):
-#     st.switch_page("Login.py")
+            "Please enter passkey before fetching data!"
+    # if st.button("Test Login"):
+    #     st.switch_page("Login.py")
+else:
+    "No data stored with us yet!"
